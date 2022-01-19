567ab02d36d9cb3af7bf2fd38bd4d152d223864e
adriel <adriel.lins@ufpe.br>
2022-01-18 23:36:45 -0300

    initial commit

---
+++ tools/grief/grief.h
@@ -0,0 +1,42 @@
+#include <sys/time.h>
+#include <stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d.hpp>
+#include <stdio.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <fstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/flann/miniflann.hpp>
+#include "opencv2/xfeatures2d.hpp"
+namespace cv{
+
+class CV_EXPORTS GriefDescriptorExtractor : public xfeatures2d::BriefDescriptorExtractor
+{
+public:
+    static const int PATCH_SIZE = 48;
+    static const int KERNEL_SIZE = 9;
+
+    // bytes is a length of descriptor in bytes. It can be equal 16, 32 or 64 bytes.
+    GriefDescriptorExtractor( int bytes = 32 );
+
+    virtual void read( const FileNode& );
+    virtual void write( FileStorage& ) const;
+
+    virtual int descriptorSize() const;
+    virtual int descriptorType() const;
+
+    /// @todo read and write for brief
+
+//    AlgorithmInfo* info() const;
+
+protected:
+    virtual void computeImpl(const Mat& image, std::vector<KeyPoint>& keypoints, Mat& descriptors) const;
+
+    typedef void(*PixelTestFn)(const Mat&, const std::vector<KeyPoint>&, Mat&);
+
+    int bytes_;
+    PixelTestFn test_fn_;
+};
+}
+
